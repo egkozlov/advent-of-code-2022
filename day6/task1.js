b4e0d8ead54ba0ab7ce3a92a0fb7a8bfeca4b762
Yegor Kozlov <e.kozlov@softteco.com>
2022-12-06 10:09:18 +0300

    Add day6 solution

---
+++ day6/task1.js
@@ -0,0 +1,20 @@
+const { readFile } = require('../shared.js');
+const _ = require('lodash');
+const inputData = readFile('./day6/input.txt');
+
+const areAllLettersDifferent = (line) => {
+  const letters = line.split('');
+  return _.uniq(letters).length === letters.length;
+};
+
+let result;
+
+for (let i = 0; i < inputData.length; i++) {
+  const sublineToCheck = inputData.slice(i, i + 4);
+  if (areAllLettersDifferent(sublineToCheck)) {
+    result = i + 4;
+    break;
+  }
+}
+
+console.log({ result });
